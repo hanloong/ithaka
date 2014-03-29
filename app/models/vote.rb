129abea881b92a20348aa6a69fabc903e8bc4676
Han Loong Liauw <hanloongliauw@gmail.com>
2014-03-29 21:58:30 +1100

    fix error

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,6 +3,6 @@ class Vote < ActiveRecord::Base
   belongs_to :user
 
   validates :idea, :user, presence: true
-  validates :idea, uniqueness: { scope: :user },
-                   message: 'You can only vote an idea once'
+  validates :idea, uniqueness: { scope: :user,
+                                 message: 'You can only vote an idea once' }
 end
