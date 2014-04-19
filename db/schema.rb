62636aa7dfdcaae2cda4a8447a4e2c8717fbc165
Han Loong Liauw <hanloongliauw@gmail.com>
2014-04-19 11:32:56 +1000

    initial styles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140417110127) do
+ActiveRecord::Schema.define(version: 20140419011512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20140417110127) do
     t.integer  "project_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "is_negative", default: false
   end
 
   add_index "factors", ["area_id"], name: "index_factors_on_area_id", using: :btree
