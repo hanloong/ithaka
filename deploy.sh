869f822ced01b5e1a889429c656ce53a5f14a24d
Han Loong Liauw <hanloongliauw@gmail.com>
2014-07-08 17:03:25 +1000

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ $BRANCH == "master" ]
+if [ "$BRANCH" == "master" ]
 then
   echo "Deploying ithka $BRANCH"
   curl "https://portal.ninefold.com/api/v1/apps/8108/redeploy?auth_token=4MMIGXynd-lXbU0HHJYpyg"
