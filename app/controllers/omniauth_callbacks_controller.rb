7a178a77d49121012760fae4f1cf884d8df5a688
Han Loong Liauw <hanloongliauw@gmail.com>
2014-07-21 21:05:33 +1000

    Can now login via google

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,9 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    raise request.env["omniauth.auth"]
+    if user = User.from_omniauth(request.env["omniauth.auth"])
+      sign_in_and_redirect user
+    else
+      redirect_to new_user_session_path, alert: 'Ooops, looks like you dont have a valid user account'
+    end
   end
 end
