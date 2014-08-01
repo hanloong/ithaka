f1eb75a460414104f7a1ee37c765c551962019fa
Han Loong Liauw <hanloongliauw@gmail.com>
2014-08-01 20:47:42 +1000

    tour stage 1

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
   before_action :update_sanitized_params, if: :devise_controller?
 
+  def after_sign_up_path_for(resource)
+    "#{projects_path}/?first_time=true"
+  end
+
   def update_sanitized_params
     devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit(:name, :email, :password,
