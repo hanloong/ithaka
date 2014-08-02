806c063f90374ecb5cf4c831c65b37115bba6a60
Han Loong Liauw <hanloongliauw@gmail.com>
2014-08-02 17:20:28 +1000

    added content helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -60,4 +60,16 @@ class User < ActiveRecord::Base
   def welcome_email
     UserMailer.delay.welcome_email(self)
   end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
