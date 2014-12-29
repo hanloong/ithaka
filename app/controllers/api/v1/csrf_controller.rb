4b0764450c36de2c44d49e539f94eb3a0198df3f
Han Loong Liauw <hanloongliauw@gmail.com>
2014-12-29 23:55:55 +1100

    added cors to csrf

--- app/controllers/api/v1/csrf_controller.rb
+++ app/controllers/api/v1/csrf_controller.rb
@@ -1,4 +1,6 @@
-class Api::V1::CsrfController < ApplicationController
+class Api::V1::CsrfController < ApiController
+  skip_before_action :authenticate_user!
+
   def index
     render json: { request_forgery_protection_token => form_authenticity_token }.to_json
   end
