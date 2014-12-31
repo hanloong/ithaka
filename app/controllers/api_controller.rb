3d28c61a2283d6a66a246f047cb8c28a29839410
Han Loong Liauw <hanloongliauw@gmail.com>
2014-12-31 15:45:50 +1100

    quick deploy

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,3 @@
 class ApiController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user_from_token!
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,7 @@ class ApplicationController < ActionController::Base
       user_email = options[:user_email].presence
       user       = user_email && User.find_by(email: user_email)
 
-      if user && Devise.secure_compare(user.authentication_token, token)
+      if user && Devise.secure_compare(user.authentication_token, token.gsub(/user_token="/, ''))
         sign_in user, store: false
       end
     end
